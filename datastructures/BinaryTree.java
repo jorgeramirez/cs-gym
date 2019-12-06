872be3d40264a66ee0878a0032b49e1d9f2a49e8
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-12-06 13:03:35 +0100

    Add tree traversal algorithm
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- datastructures/BinaryTree.java
+++ datastructures/BinaryTree.java
@@ -1,5 +1,9 @@
 package datastructures;
 
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 
 
@@ -7,11 +11,93 @@ public class BinaryTree {
         return null;
     }
 
+    public void printInOrder(TreeNode node) {
+        // In order: left, root, right
+        // if the tree is a BST then these print elements in ascending order
+        // Time O(n) Space O(n)
+        if(node == null) {
+            return;
+        }
+        printInOrder(node.left);
+        System.out.print(node.val + " ");
+        printInOrder(node.right);
+    }
+
+    public void printPreOrder(TreeNode node) {
+        // Pre order: root, left, right
+        // useful to create a copy of a Tree (because we need to create the parent first, then the children)
+        // Time O(n) Space O(n)
+        if(node == null) {
+            return;
+        }
+        System.out.print(node.val + " ");
+        printPreOrder(node.left);
+        printPreOrder(node.right);
+    }
+
+    public void printPostOrder(TreeNode node) {
+        // Post Order: Left, Right, Root
+        // use full to delete a Tree (because we remove the children first and then the parent)
+        // Time O(n) Space O(n)
+        if(node == null) {
+            return;
+        }
+        printPostOrder(node.left);
+        printPostOrder(node.right);
+        System.out.print(node.val + " ");
+    }
+
+    public void printLevelOrder(TreeNode root) {
+        // Breadth-first printing
+        // Time O(n) Space O(n)
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode current;
+        while(!queue.isEmpty()) {
+            current = queue.poll();
+            System.out.print(current.val + " ");
+
+            if(current.left != null) {
+                queue.add(current.left);
+            }
+
+            if(current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         BinaryTree bt = new BinaryTree();
-        int[] nodes = new int[] {1, 2, 3, 4, 5, Integer.parseInt(null), 6};
-        TreeNode root = bt.buildFromArray(nodes);
+        //int[] nodes = new int[] {1, 2, 3, 4, 5, Integer.parseInt(null), 6};
+        //TreeNode root = bt.buildFromArray(nodes);
+
+// Input tree
+//                 1
+//                / \
+//               3   2
+//              /     \
+//             5       9
+//            /         \
+//           6           7
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(5);
+        root.left.left.left = new TreeNode(6);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(9);
+        root.right.right.right = new TreeNode(7);
+
+
+        System.out.println("In order");
+        bt.printInOrder(root);
+        System.out.println("\nPre order");
+        bt.printPreOrder(root);
+        System.out.println("\nPost order");
+        bt.printPostOrder(root);
+        System.out.println("\nLevel ordering");
+        bt.printLevelOrder(root);
     }
 
     public static class TreeNode {
